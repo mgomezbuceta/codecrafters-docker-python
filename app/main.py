d75125b49599dcd245bdb704d26ee3497e57522b
Marcos Gómez Buceta <m.gomez@sixtema.es>
2024-05-19 17:15:09 +0200

    feat: Sanitize command and arguments in main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 import subprocess
 import sys
-
+import shlex
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
 
-    completed_process = subprocess.run([command, *args], capture_output=True)
+    sanitized_command = shlex.quote(command)
+    sanitized_args = [shlex.quote(arg) for arg in args]
+
+    completed_process = subprocess.run([sanitized_command, *sanitized_args], capture_output=True)
     print(completed_process.stdout.decode("utf-8"))
 
 if __name__ == "__main__":
